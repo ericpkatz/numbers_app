0ab2af98476682973675df2ca55948f79888ec53
ericpkatz <ericpkatz@gmail.com>
2023-12-07 10:41:20 -0500

    working app

---
+++ index.js
@@ -0,0 +1,23 @@
+const numbers = [1, 2, 3];
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+
+form.addEventListener('submit', function(ev){
+  ev.preventDefault();
+  numbers.push(input.value*1);
+  render();
+});
+
+function render(){
+  let html = numbers.map(function(num){
+    return `
+      <li>${num}</li>
+    `;
+  });
+  html = html.join('');
+  ul.innerHTML = html;
+}
+
+render();
